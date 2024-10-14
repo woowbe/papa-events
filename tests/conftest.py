2d676d918f7dc44a39ca9cd9d86cb8c082c0d70e
Cesar Gonzalez <cesar@woowbe.com>
2024-10-14 14:50:09 +0200

    :white_check_mark: add some tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,11 @@
 import pytest
 from testcontainers.rabbitmq import RabbitMqContainer
 
+from papa_events import config
+
+
 @pytest.fixture(scope="session")
 def rabbitmq_container():
+    config.settings.timeout = 1
     with RabbitMqContainer("rabbitmq:4") as rabbitmq:
         yield rabbitmq
