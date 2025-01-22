f4f38a40547c583da379d2f4ba25ccc5f9618933
Cesar Gonzalez <cesar@woowbe.com>
2025-01-22 08:11:51 +0100

    :sparkles: support multiple rabbitmq versions

--- src/papa_events/config.py
+++ src/papa_events/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     max_jobs: int = 20
     retries: int = 5
     timeout: int = 10  # seconds
+    rabbitmq_version: int = 4
     model_config = SettingsConfigDict(env_prefix="papa_events_")
 
 
